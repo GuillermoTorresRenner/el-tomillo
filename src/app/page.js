7f5028b4b4274d0e108b0a3aa74f96ecdec9539d
LeBateleur <torresrennerguillermo@gmail.com>
2024-04-06 04:45:30 -0300

    Guia de estilos iniciales para verificación

--- src/app/page.js
+++ src/app/page.js
@@ -65,16 +65,14 @@ export default function Home() {
             <td className="border border-gray-400  py-2 px-10">7</td>
             <td className="border border-gray-400  py-2 px-10">Subhead 1</td>
             <td className="border border-gray-400  py-2 px-10">
-              <p className="subhead-1">Subhead 2</p>
+              <p className="subhead-1">Subhead 1</p>
             </td>
           </tr>
           <tr>
             <td className="border border-gray-400  py-2 px-10">8</td>
             <td className="border border-gray-400  py-2 px-10">Subhead 2</td>
             <td className="border border-gray-400  py-2 px-10">
-              <h1>
-                <p className="subhead-2">Subhead 2</p>
-              </h1>
+              <p className="subhead-2">Subhead 2</p>
             </td>
           </tr>
           <tr>
