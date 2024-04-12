9b18535f697afcb179b538607cf7dcf91a244c48
LeBateleur <torresrennerguillermo@gmail.com>
2024-04-12 01:41:41 -0400

    resolución de conflictos de merge Ariel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,20 +24,24 @@ export default function Navbar() {
       if (ww >= 768) {
         setIsMobile(false);
         if (isOpen) {
-          // setNavClasses("flex justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row")
-          setNavClasses("w-full");
+          setNavClasses(
+            "flex justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row"
+          );
         } else {
-          // setNavClasses("hidden justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row")
-          setNavClasses("w-full");
+          setNavClasses(
+            "hidden justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row"
+          );
         }
       } else {
         setIsMobile(true);
         if (isOpen) {
-          // setNavClasses("w-full flex flex-col items-center justify-start ")
-          setNavClasses("w-full");
+          setNavClasses(
+            "w-full py-2 mt-40 relative top-20 flex flex-col items-center justify-start bg-black bg-opacity-50 backdrop-blur-sm backdrop-filter"
+          );
         } else {
-          // setNavClasses("hidden justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row")
-          setNavClasses("w-full");
+          setNavClasses(
+            "hidden justify-center items-center gap-x-8 space-x-4 flex-col lg:flex lg:flex-row"
+          );
         }
       }
     }
@@ -48,31 +52,33 @@ export default function Navbar() {
   };
 
   return (
-    <>
-      <Image
-        src="/images/index_header_img.png"
-        alt="header background image"
-        width={2000}
-        height={200}
-      />
+    <nav class="relative">
+      <div class="">
+        <Image
+          src="/images/index_header_bg_img.webp"
+          alt="header background image"
+          width={2000}
+          height={200}
+        />
+      </div>
 
-      <nav class="fixed top-0 bg-black bg-opacity-5 backdrop-blur-sm backdrop-filter shadow-lg px-8 flex justify-between items-center w-full h-20">
+      <div class="absolute top-0 bg-black bg-opacity-35 backdrop-blur-sm backdrop-filter shadow-lg px-8 flex justify-between items-center w-full h-20">
         <div className="">
-          <a href="/">
+          <a href="#">
             <Image
-              src="/images/link_home_img.svg"
+              src="/images/eltomillo_header_img.svg"
               alt="home link image"
-              width={100}
-              height={100}
+              width={70}
+              height={70}
             />
           </a>
         </div>
 
-        <div className="w-full">
+        <div className="relative flex w-full justify-end">
           <ul class={navClasses}>
             <li>
               <Link
-                href="/"
+                href={"#"}
                 className="text-caption text-white hover:underline"
               >
                 {" "}
@@ -81,7 +87,7 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                href="/servicios"
+                href="#"
                 className="text-caption text-white hover:underline"
               >
                 {" "}
@@ -90,7 +96,7 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                href="/productos"
+                href="#"
                 className="text-caption text-white hover:underline"
               >
                 {" "}
@@ -99,7 +105,7 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                href="/actividades"
+                href="#"
                 className="text-caption text-white hover:underline"
               >
                 {" "}
@@ -109,7 +115,7 @@ export default function Navbar() {
             <li>
               <Link
                 href="#"
-                className="flex justify-center items-center border border-1 rounded-md px-6 py-2 hover:border-primary hover:bg-secondary text-white text-caption hover:text-black"
+                className="flex justify-center items-center border border-1 rounded-lg px-6 py-2 hover:border-primary hover:bg-secondary text-white text-caption hover:text-black"
               >
                 {" "}
                 Contacto{" "}
@@ -121,7 +127,7 @@ export default function Navbar() {
         <div className="block lg:hidden">
           <button
             type="button"
-            class="bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 rounded-lg text-sm px-.5 py-.5 dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
+            class="bg-[#24292F] hover:bg-[#24292F]/90 rounded-lg text-sm px-.5 py-.5 dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
             onClick={() => {
               toggleVisibility();
             }}
@@ -134,7 +140,7 @@ export default function Navbar() {
             />
           </button>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
