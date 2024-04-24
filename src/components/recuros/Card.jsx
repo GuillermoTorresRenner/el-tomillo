e88fdc1a15117c9c03908bdd66d313594b9e99bd
LeBateleur <torresrennerguillermo@gmail.com>
2024-04-23 22:59:49 -0400

    cración de página 404

--- src/components/recuros/Card.jsx
+++ src/components/recuros/Card.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Card = ({ src, alt, title, text, pdf }) => {
   return (
-    <div className="border border-gray-400 rounded-2xl my-5 relative my-5 sm:my-10">
+    <div className="border border-gray-400 rounded-2xl my-5 relative  sm:my-10">
       <div className="relative h-full">
         <Image src={src} alt={alt} className="w-full h-full object-cover" />
         <h2 className="absolute bottom-10 left-4 text-white text-2xl lg:text-8xl w-3/5 md:1/4">
