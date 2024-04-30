dc7d9d3e9e4c3060b171f297e7d18c7db7833f76
LeBateleur <torresrennerguillermo@gmail.com>
2024-04-30 00:08:21 -0400

    Se termina de arreglar la page recursos

--- src/app/recursos/page.jsx
+++ src/app/recursos/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../../components/recuros/Card";
+import Card from "../../components/recursos/Card";
 import Img1 from "../../../public/images/recursos/image_card_1.png";
 import Img2 from "../../../public/images/recursos/image_card_2.png";
 import Img3 from "../../../public/images/recursos/image_card_3.png";
@@ -21,7 +21,9 @@ const Recursos = () => {
           src={Img1}
           alt={"Cómo comenzar a compostar"}
           title={"Cómo comenzar a compostar"}
-          text={"lorem ipsum dolor sit amet, consectetur adip"}
+          text={
+            "En este E-book conocerás unos sencillos tips para que puedas realizar tu propio compost desde casa."
+          }
           pdf={
             "https://drive.google.com/file/d/1aykN5OTwBPPaLq1DM2x6HCFIMl0FhpkH/view?usp=sharing"
           }
@@ -30,7 +32,9 @@ const Recursos = () => {
           src={Img2}
           alt={"Huerta Fácil"}
           title={"Huerta Fácil"}
-          text={"lorem ipsum dolor sit amet, consectetur adip"}
+          text={
+            "En este E-Book conocerás 5 sencillos pasos para realizar tu huerta en un cantero."
+          }
           pdf={
             "https://drive.google.com/file/d/1vKqMqUGDtJjYDO-izeQrV3OoNwBngUUU/view?usp=sharing"
           }
@@ -39,7 +43,9 @@ const Recursos = () => {
           src={Img3}
           alt={"Cambio de estación en la huerta"}
           title={"Cambio de estación en la huerta"}
-          text={"lorem ipsum dolor sit amet, consectetur adip"}
+          text={
+            "Conoce los pasos que debes realizar cuando llegue el cambio de estación en tu huerta."
+          }
           pdf={
             "https://drive.google.com/file/d/1Za-juAet17FrE57a691VflSEWeerWGA3/view?usp=sharing"
           }
