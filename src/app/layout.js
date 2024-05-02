2678947caa7772a827d018660eaaeaf7a65b963e
LeBateleur <torresrennerguillermo@gmail.com>
2024-05-01 23:48:27 -0400

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <script defer="" src="https://cdn.tailwindcss.com"></script>
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"
